414af3ca42d3000d9624814fc7da75b17500935c
Anamika Kumari <anamika080300@gmail.com>
2025-10-06 16:54:35 +0530

    dashboard

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",  // ✅ Tells Tailwind to scan all JS/JSX files in src
+    "./src/**/*.{js,jsx,ts,tsx}",  // Tells Tailwind to scan all JS/JSX files in src
   ],
   theme: {
     extend: {},
