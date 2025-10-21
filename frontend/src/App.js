ddd3751c9a54b4518993b9286d8306c36b103785
jayasrisai2004 <159995240+jayasrisai2004@users.noreply.github.com>
2025-10-21 12:04:19 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
+
+
 import React from "react";
+import 'leaflet/dist/leaflet.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard"; // citizen dashboard
+import AdminDashboard from "./pages/AdminDashboard";
 import ForgotPassword from "./pages/ForgotPassword";
-
-
-
+import ComplaintCreate from './pages/ComplaintCreate';
 
 function App() {
   return (
@@ -15,11 +17,15 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} /> {/* citizen */}
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/complaints/new" element={<ComplaintCreate />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+
